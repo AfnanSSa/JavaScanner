01f35b9be381fdb70329f13f140ec15fb3726949
Afnan Sulaiman <114135275+AfnanSSa@users.noreply.github.com>
2024-02-15 10:02:28 +0400

    Printing first letter or last letter

---
+++ FirstOrLast.java
@@ -0,0 +1,32 @@
+/*Given a string, return a string length 1 from its front, unless front is false,
+in which case return a string length 1 from its back. The string will be non-empty.
+
+theEnd("Hello", true) → "H"
+theEnd("Hello", false) → "o"
+theEnd("oh", true) → "o"*/
+
+import java.util.Scanner;
+
+public class ScannerTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Prompting user to enter a words
+        System.out.println("Enter a word: ");
+        String word = scanner.nextLine();
+        System.out.println("Enter 1 to extract first letter form the word.\nEnter 0 to extract last letter from the word: ");
+        int option = scanner.nextInt();
+
+        if(option==1){
+            //Extracting first letter
+            String onlyFirst = word.substring(0,1);
+            System.out.println("\n\"" + onlyFirst + "\"");
+        }else if (option==0){
+            //Removing last letter
+            String onlyLast = word.substring(word.length()-1);
+            System.out.println("\n\"" + onlyLast + "\"");
+        }else{
+            System.out.println("\nEnter only 0 or 1");
+        }
+
+    }
+}
